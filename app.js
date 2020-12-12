4bf70a6d9f38c58d14ae8b6adf75e50b9b8a71db
jerry <jiahuasir@gmail.com>
2020-12-12 17:11:14 +0800

    Add koa-bodyparser

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const koa = require('koa');
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 const app = new koa();
+
+app.use(bodyParser());
+
+app.use(async (ctx, next) => {
+    next();
+    if (ctx.status == 404) {
+        ctx.status = 404;
+        ctx.body = '404'
+    }
+})
 
 router.get('/', async (ctx) => {
     ctx.body = 'hi koa'
 })
 
 router.get('/list', async (ctx) => {
-    ctx.body = 'list page'
+    let r = ctx.query;
+    ctx.body = r
+})
+
+router.get('/detail/:id/:key', async (ctx) => {
+    let r = ctx.params;
+    ctx.body = r
+})
+
+router.post('/login', async (ctx) => {
+    ctx.body = ctx.request.body
 })
 
 // Start router
